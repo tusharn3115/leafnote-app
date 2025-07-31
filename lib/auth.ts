31e5c965d4134e7eb4cb53624561b01da2e6eee7
tusharn3115 <negitushar81@gmail.com>
2025-07-31 19:47:24 +0530

    implemented google auth and creating side bar for dashboard

--- lib/auth.ts
+++ lib/auth.ts
@@ -35,6 +35,13 @@ export const auth = betterAuth({
     sendOnSignUp: true,
   },
 
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
